16c558610f362fd0a8b4221db1fb9bcf643b878a
Seidy Kante <seidykante21@gmail.com>
2025-04-08 15:41:45 +0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  darkMode: "class", // Enable dark mode via a CSS class
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
